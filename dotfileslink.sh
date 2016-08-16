76abf58bf883d6e293c799bd5380f68b640ee2e1
Hideaki Nire <hideaki@nire.com>
2016-08-16 21:05:58 +0900

    added .bashrc to display git branch

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.atom/config.cson ~/.atom/config.cson
 ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
