a927667dd7c7c89eaf3839ea5f0a247bd4ebf4ed
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2016-08-17 23:34:42 +0900

    Revert "added .gitconfig"
    This reverts commit 2522447a9be5f8effba9f7581b2446ed22bae669.
    
    canceled to add .gitconfig
    
    modified user and mail

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.atom/config.cson ~/.atom/
-ln -sf ~/dotfiles/.bashrc ~
-ln -sf ~/dotfiles/.gitconfig ~
-ln -sf ~/dotfiles/.editorconfig ~
-ln -sf ~/dotfiles/.vimrc ~
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.atom/config.cson ~/.atom/config.cson
+ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
