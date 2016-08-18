82d9e1df6ca786b1ace9ffe1a017018a9d794a60
Hideaki Nire <hideaki@nire.com>
2016-08-18 19:55:37 +0900

    added .atom/keymap.cson

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.atom/config.cson ~/.atom/config.cson
-ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
+ln -sf ~/dotfiles/.bashrc ~/
+ln -sf ~/dotfiles/.vimrc ~/
+ln -sf ~/dotfiles/.atom/config.cson ~/.atom/
+ln -sf ~/dotfiles/.atom/keymap.cson ~/.atom/
+ln -sf ~/dotfiles/.editorconfig ~/
