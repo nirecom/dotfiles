3730aeee1d9624f8f1ae2b310fb859a0436995b8
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2016-08-19 23:52:45 +0000

    compliant for ubuntu terminal

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.bashrc ~/
 ln -sf ~/dotfiles/.vimrc ~/
+mkdir ~/.atom/
 ln -sf ~/dotfiles/.atom/config.cson ~/.atom/
 ln -sf ~/dotfiles/.atom/keymap.cson ~/.atom/
 ln -sf ~/dotfiles/.editorconfig ~/
