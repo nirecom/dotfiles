c5f44f58bc1ac3b8af731a784f3485050fd932e9
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2016-08-20 09:07:13 +0900

    added .gitconfig

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.bashrc ~/
 ln -sf ~/dotfiles/.vimrc ~/
-mkdir ~/.atom/
+ln -sf ~/dotfiles/.editorconfig ~/
+ln -sf ~/dotfiles/.gitconfig ~/
+mkdir -p ~/.atom/
 ln -sf ~/dotfiles/.atom/config.cson ~/.atom/
 ln -sf ~/dotfiles/.atom/keymap.cson ~/.atom/
-ln -sf ~/dotfiles/.editorconfig ~/
