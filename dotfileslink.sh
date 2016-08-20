0693901dee43871ceed9da01546296f0324fc562
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2016-08-20 03:55:34 +0000

    old ssh-agent script + new keychain

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.bashrc ~/
+#ln -sf ~/dotfiles/.bash_profile ~/
 ln -sf ~/dotfiles/.vimrc ~/
 ln -sf ~/dotfiles/.editorconfig ~/
 ln -sf ~/dotfiles/.tmux.conf ~/
-#ln -sf ~/dotfiles/.gitconfig ~/
+ln -sf ~/dotfiles/.gitconfig ~/
 mkdir -p ~/.atom/
 ln -sf ~/dotfiles/.atom/config.cson ~/.atom/
 ln -sf ~/dotfiles/.atom/keymap.cson ~/.atom/
