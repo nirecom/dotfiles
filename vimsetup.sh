78f27463cc6f37af4c5afa80b60627e5430345d5
Hideaki Nire <hideaki@nire.com>
2016-08-21 13:59:15 +0900

    added .vim setup

---
+++ vimsetup.sh
@@ -0,0 +1,9 @@
+# pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+# plugins
+git clone git://github.com/tpope/vim-sensible.git
+git clone git@github.com:editorconfig/editorconfig-vim.git
+git clone git://github.com/altercation/vim-colors-solarized.git
+git clone https://github.com/elzr/vim-json.git
