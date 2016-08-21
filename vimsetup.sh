83caf04cbeb33aa25d25f196b0dbbfb44f7dcad6
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2016-08-21 05:09:45 +0000

    vimsetup.sh: added cloudformation .template

--- vimsetup.sh
+++ vimsetup.sh
@@ -6,4 +6,7 @@ cd ~/.vim/bundle
 git clone git://github.com/tpope/vim-sensible.git
 git clone git@github.com:editorconfig/editorconfig-vim.git
 git clone git://github.com/altercation/vim-colors-solarized.git
+
+# vim-json
 git clone https://github.com/elzr/vim-json.git
+ln -sf ~/dotfiles/filetype.vim ~/.vim/
