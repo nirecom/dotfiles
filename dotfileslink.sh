fcd40f4535a407ba09702e5dd330f822604cc25a
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2020-12-20 01:12:08 +0000

    modified .gitconfig

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.bashrc ~/
+#ln -sf ~/dotfiles/.bashrc ~/
 ln -sf ~/dotfiles/.bash_profile ~/
 ln -sf ~/dotfiles/.vimrc ~/
 ln -sf ~/dotfiles/.editorconfig ~/
 ln -sf ~/dotfiles/.tmux.conf ~/
 ln -sf ~/dotfiles/.gitconfig ~/
-mkdir -p ~/.atom/
-ln -sf ~/dotfiles/.atom/config.cson ~/.atom/
-ln -sf ~/dotfiles/.atom/keymap.cson ~/.atom/
+if [ -d ~/.atom/ ]; then
+    ln -sf ~/dotfiles/.atom/config.cson ~/.atom/
+    ln -sf ~/dotfiles/.atom/keymap.cson ~/.atom/
+fi
+if [ -d ~/.emacs.d/ ]; then
+    ln -sf ~/dotfiles/.init.el ~/.emacs.d/
+fi
