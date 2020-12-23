b21a9899acd3b1a2d28eed8a4ab69d5ebd3387e6
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2020-12-23 12:13:48 +0000

    split kubernetes.sh into two part: docker-19.03 and kubernetes.sh

---
+++ setup/kubernetes/docker-19.03.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Install Docker
+# ref. https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+echo Install Docker that matches with kubernetes .....
+sudo apt-get update && sudo apt-get install -y \
+  apt-transport-https ca-certificates curl software-properties-common gnupg2
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add -
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+# Install Docker CE
+sudo apt-get update && sudo apt-get install -y --allow-downgrades \
+  containerd.io=1.2.13-2 \
+  docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) \
+  docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
+# Set up the Docker daemon
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+sudo mkdir -p /etc/systemd/system/docker.service.d
+# Restart Docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+# Start Docker on boot
+sudo systemctl enable docker
+echo Finished Docker setup
+echo You may want to run following command to cleanup unnecessary libs
+echo sudo apt autoremove
