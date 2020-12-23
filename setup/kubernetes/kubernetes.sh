66f53d3e02bb385ece434dd38695b08e1efe3bb9
Hideaki Nire (nirecom) <hideaki@nire.com>
2020-12-23 12:13:48 +0000

    split kubernetes.sh into two part: docker-19.03 and kubernetes.sh

--- setup/kubernetes/kubernetes.sh
+++ setup/kubernetes/kubernetes.sh
@@ -1,44 +1,14 @@
 #!/bin/bash
-# Install Docker
-# ref. https://kubernetes.io/docs/setup/production-environment/container-runtimes/
-echo Install Docker .....
-sudo apt-get update && sudo apt-get install -y \
-  apt-transport-https ca-certificates curl software-properties-common gnupg2
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add -
-sudo add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
-# Install Docker CE
-sudo apt-get update && sudo apt-get install -y --allow-downgrades \
-  containerd.io=1.2.13-2 \
-  docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) \
-  docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
-# Set up the Docker daemon
-cat <<EOF | sudo tee /etc/docker/daemon.json
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-sudo mkdir -p /etc/systemd/system/docker.service.d
-# Restart Docker
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-# Start Docker on boot
-sudo systemctl enable docker
+#
 # Install Kubernetes
 # ref. https://qiita.com/nnagashima/items/d7deb00d086b6e276eea
+#
+# Run docker-19.03.sh first to match with this version of Kubernetes
 echo Install Kubernetes .....
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 #sudo apt install apt-transport-https curl
 # was not required on Ubuntu 20.04
 sudo apt update
-sudo apt install kubeadm
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt install kubeadm kubelet kubectl kubernetes-cni
