3812340bed8d787f766155ae6249612b9473fefc
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-01-17 00:16:54 +0000

    k8s: client.sh copies .kube files as well

--- setup/k8s/client.sh
+++ setup/k8s/client.sh
@@ -4,7 +4,7 @@
 # ref. https://qiita.com/nnagashima/items/d7deb00d086b6e276eea
 #
 # Run docker-19.03.sh first to match with this version of Kubernetes
-echo Install Kubernetes .....
+echo Install Kubernetes Client .....
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 #sudo apt install apt-transport-https curl
 # was not required on Ubuntu 20.04
@@ -12,4 +12,7 @@ sudo apt update
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt install -y kubectl
+echo "Copying config to $HOME/.kube ..."
+mkdir -p $HOME/.kube
+aws s3 cp s3://nirecom-home/.kube/config $HOME/.kube/
 #swapoff -a
