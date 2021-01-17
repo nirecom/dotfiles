d074b398128e0600ee44412d136f49ee3e48cde7
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2021-01-17 14:34:31 +0000

    bugfixes

--- setup/sshd.sh
+++ setup/sshd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Allow Agentforwarding to SSH
-grep -q -E '^ *AllowAgentForwarding' /etc/ssh/sshd_config
+sudo grep -q -E '^ *AllowAgentForwarding' /etc/ssh/sshd_config
 if [ $? -gt 0 ]; then
 	echo "Adding AllowAgentForwarding ..."
 	sudo sh -c 'echo "#nn Added by sshd.sh" >>/etc/ssh/sshd_config'
