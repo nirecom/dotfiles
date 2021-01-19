15164a137f43dd336803014730043e70c87cc65a
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-01-19 01:16:45 +0000

    bugfix

--- setup/emacs.sh
+++ setup/emacs.sh
@@ -2,9 +2,9 @@
 # Install emacs
 # ref. https://qiita.com/MasahiroBW/items/f263e7a3dcfe69ec0561
 sudo apt install emacs
-if [ ! ~/tmp ]; then
+if [ ! -d ~/tmp ]; then
     mkdir ~/tmp
 fi
-if [ ! ~/.emacs_backup ]; then
+if [ ! -d ~/.emacs_backup ]; then
     mkdir ~/.emacs_backup
 fi
