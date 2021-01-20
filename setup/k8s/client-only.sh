c4805259309828ac55187a229fb9f491ef2a3c9e
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-01-20 03:41:01 +0000

    enhanced client-only.sh to check if exists

--- setup/k8s/client-only.sh
+++ setup/k8s/client-only.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 #
-# Install Kubernetes
+# Install Kubernetes Client
 # ref. https://qiita.com/nnagashima/items/d7deb00d086b6e276eea
 #
-# Run docker-19.03.sh first to match with this version of Kubernetes
-echo Install Kubernetes Client .....
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-#sudo apt install apt-transport-https curl
-# was not required on Ubuntu 20.04
-sudo apt update
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-sudo apt install -y kubectl
-echo "Copying config to $HOME/.kube ..."
-mkdir -p $HOME/.kube
-aws s3 cp s3://nirecom-home/.kube/config $HOME/.kube/
+if type kubectl > /dev/null 2>&1; then
+    echo "kubectl exists. Skip installation..."
+else
+    # Run docker-19.03.sh first to match with this version of Kubernetes
+    echo Install Kubernetes Client .....
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+    #sudo apt install apt-transport-https curl
+    # was not required on Ubuntu 20.04
+    sudo apt update
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+    sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+    sudo apt install -y kubectl
+fi
+
+if [ -d "$HOME/.kube" ]; then
+    echo "$HOME/.kube exists. Skip copying ..."
+else
+    echo "Copying config to $HOME/.kube ..."
+    mkdir -p $HOME/.kube
+    aws s3 cp s3://nirecom-home/.kube/config $HOME/.kube/
+fi
 #swapoff -a
