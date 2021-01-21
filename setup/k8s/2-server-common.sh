d8da95cfd590601b951c398098fe4a436fb37b49
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2021-01-21 23:55:11 +0000

    added 3-etc-first-host.sh

--- setup/k8s/2-server-common.sh
+++ setup/k8s/2-server-common.sh
@@ -27,3 +27,17 @@ else
     sudo apt install -y kubeadm kubelet kubectl kubernetes-cni
 fi
 swapoff -a
+
+#
+# Install etcd: Common Part
+#
+cat << EOF | sudo tee /etc/systemd/system/kubelet.service.d/20-etcd-service-manager.conf
+[Service]
+ExecStart=
+#  Replace "systemd" with the cgroup driver of your container runtime. The default value in the kubelet is "cgroupfs".
+ExecStart=/usr/bin/kubelet --address=127.0.0.1 --pod-manifest-path=/etc/kubernetes/manifests --cgroup-driver=systemd
+Restart=always
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
