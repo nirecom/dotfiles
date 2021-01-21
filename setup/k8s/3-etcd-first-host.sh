57eb1964314e0003935ee7f6b8fad188c896a5bf
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-01-21 23:55:11 +0000

    added 3-etc-first-host.sh

---
+++ setup/k8s/3-etcd-first-host.sh
@@ -0,0 +1,75 @@
+# Update HOST0, HOST1, and HOST2 with the IPs or resolvable names of your hosts
+export HOST0=k8s-master-ne1a
+export HOST1=k8s-master-ne1c
+export HOST2=k8s-master-ne1d
+
+# Create temp directories to store files that will end up on other hosts.
+mkdir -p /tmp/${HOST0}/ /tmp/${HOST1}/ /tmp/${HOST2}/
+
+ETCDHOSTS=(${HOST0} ${HOST1} ${HOST2})
+NAMES=("infra0" "infra1" "infra2")
+
+for i in "${!ETCDHOSTS[@]}"; do
+HOST=${ETCDHOSTS[$i]}
+NAME=${NAMES[$i]}
+cat << EOF > /tmp/${HOST}/kubeadmcfg.yaml
+apiVersion: "kubeadm.k8s.io/v1beta2"
+kind: ClusterConfiguration
+etcd:
+    local:
+        serverCertSANs:
+        - "${HOST}"
+        peerCertSANs:
+        - "${HOST}"
+        extraArgs:
+            initial-cluster: ${NAMES[0]}=https://${ETCDHOSTS[0]}:2380,${NAMES[1]}=https://${ETCDHOSTS[1]}:2380,${NAMES[2]}=https://${ETCDHOSTS[2]}:2380
+            initial-cluster-state: new
+            name: ${NAME}
+            listen-peer-urls: https://${HOST}:2380
+            listen-client-urls: https://${HOST}:2379
+            advertise-client-urls: https://${HOST}:2379
+            initial-advertise-peer-urls: https://${HOST}:2380
+EOF
+done
+
+#
+# Generate the certificate authority
+# ref. https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/setup-ha-etcd-with-kubeadm/
+#
+sudo kubeadm init phase certs etcd-server --config=/tmp/${HOST2}/kubeadmcfg.yaml
+sudo kubeadm init phase certs etcd-peer --config=/tmp/${HOST2}/kubeadmcfg.yaml
+sudo kubeadm init phase certs etcd-healthcheck-client --config=/tmp/${HOST2}/kubeadmcfg.yaml
+sudo kubeadm init phase certs apiserver-etcd-client --config=/tmp/${HOST2}/kubeadmcfg.yaml
+sudo cp -R /etc/kubernetes/pki /tmp/${HOST2}/
+# cleanup non-reusable certificates
+sudo find /etc/kubernetes/pki -not -name ca.crt -not -name ca.key -type f -delete
+
+sudo kubeadm init phase certs etcd-server --config=/tmp/${HOST1}/kubeadmcfg.yaml
+sudo kubeadm init phase certs etcd-peer --config=/tmp/${HOST1}/kubeadmcfg.yaml
+sudo kubeadm init phase certs etcd-healthcheck-client --config=/tmp/${HOST1}/kubeadmcfg.yaml
+sudo kubeadm init phase certs apiserver-etcd-client --config=/tmp/${HOST1}/kubeadmcfg.yaml
+sudo cp -R /etc/kubernetes/pki /tmp/${HOST1}/
+sudo find /etc/kubernetes/pki -not -name ca.crt -not -name ca.key -type f -delete
+
+sudo kubeadm init phase certs etcd-server --config=/tmp/${HOST0}/kubeadmcfg.yaml
+sudo kubeadm init phase certs etcd-peer --config=/tmp/${HOST0}/kubeadmcfg.yaml
+sudo kubeadm init phase certs etcd-healthcheck-client --config=/tmp/${HOST0}/kubeadmcfg.yaml
+sudo kubeadm init phase certs apiserver-etcd-client --config=/tmp/${HOST0}/kubeadmcfg.yaml
+# No need to move the certs because they are for HOST0
+
+USER=nire
+SSHHOSTS=(${HOST1} ${HOST2})
+for i in "${!SSHHOSTS[@]}"; do
+  HOST=${SSHHOSTS[$i]}
+  echo "Copying cert to $HOST ..."    
+  scp -r /tmp/${HOST}/* ${USER}@${HOST}:
+done
+
+# clean up certs that should not be copied off this host
+#find /tmp/${HOST2} -name ca.key -type f -delete
+#find /tmp/${HOST1} -name ca.key -type f -delete
+
+echo "Done. Please copy files manually with:"
+echo "$USER@$HOST $ sudo -Es"
+echo "root@HOST $ chown -R root:root pki"
+echo "root@HOST $ mv pki /etc/kubernetes/"
