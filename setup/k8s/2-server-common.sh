2e9bc0a759fec4c9b58eb3840b5c06e5fc7f148a
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2021-01-21 13:44:39 +0000

    2-server-common: added bridge settings

--- setup/k8s/2-server-common.sh
+++ setup/k8s/2-server-common.sh
@@ -2,14 +2,28 @@
 #
 # Install Kubernetes
 # ref. https://qiita.com/nnagashima/items/d7deb00d086b6e276eea
-#
-# Run docker-19.03.sh first to match with this version of Kubernetes
-echo Install Kubernetes .....
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-#sudo apt install apt-transport-https curl
-# was not required on Ubuntu 20.04
-sudo apt update
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-sudo apt install -y kubeadm kubelet kubectl kubernetes-cni
+
+# Letting iptables see bridged traffic
+# ref. https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#troubleshooting
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
+
+if type "kubeadm" >/dev/null 2>&1; then
+    echo kubadm exists. Skipping installation of Kubernetes tools...
+else
+    echo Install Kubernetes .....
+    sudo apt update
+    sudo apt install -y apt-transport-https curl        
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+    sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+    sudo apt install -y kubeadm kubelet kubectl kubernetes-cni
+fi
 swapoff -a
