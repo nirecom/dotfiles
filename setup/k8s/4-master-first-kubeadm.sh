8e1aa3dce87110a763fac20bd5a5911116617469
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2021-01-22 02:37:00 +0000

    changed timing of kubeadm check

--- setup/k8s/4-master-first-kubeadm.sh
+++ setup/k8s/4-master-first-kubeadm.sh
@@ -10,15 +10,15 @@ echo "Initializing with kubeadm init ..."
 #sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=Mem
 # --control-plan-endpoint is for high-availability masters cluster
 sudo kubeadm init --control-plane-endpoint "k8s-master:16443" --upload-certs --pod-network-cidr=10.244.0.0/16 | tee ~/kubeadm-init.log.txt
+if [ $? -gt 0 ]; then
+    echo "Kubeadm init failed. Abort."
+    exit 1
+fi
 
 # You will see Docker version error with 20.0.1. Need to use 19.03
 # ref. https://qiita.com/soumi/items/7736ac3aabbbe4fb474a
 # If succeeded, you will see messages:
 # "Your Kubernetes control-plane has initialized successfully!"
-if [ $? -gt 0 ]; then
-    echo "Kubeadm init failed. Abort."
-    exit 1
-fi
 
 # Copy Kubernetes config files as instructed
 echo "Copying config to $HOME/.kube ..."
