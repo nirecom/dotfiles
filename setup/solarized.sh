7d9c8fac554b21d2fa6f826c9149e9b073f859fd
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2021-01-23 07:31:09 +0000

    separated setup folder to https://github.com/nirecom/install

--- setup/solarized.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~
-if [ ! -d solarized ]; then
-    git clone git://github.com/altercation/solarized.git
-fi
-SOLARDEST=~/solarized
-
-mkdir -p ~/.vim/colors/
-cp $SOLARDEST/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
--- setup/sudoers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Allow the user to run sudo without password
-if [ ! -f /etc/sudoers.d/nirecom-users ]; then
-    sudo sh -c 'echo "nire    ALL=NOPASSWD: ALL" >>/etc/sudoers.d/nirecom-users'
-    sudo chown root:root /etc/sudoers.d/nirecom-users
-    sudo chmod 440 /etc/sudoers.d/nirecom-users
-fi
