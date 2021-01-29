6a29aff35fe699f23684d425ec3925a7b9ef855e
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2021-01-29 12:01:44 +0000

    automatically added part of init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -161,7 +161,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(php-mode dockerfile-mode markdown-mode uuidgen solarized-theme markdown-preview-mode)))
+   (quote
+    (php-mode dockerfile-mode markdown-mode uuidgen solarized-theme markdown-preview-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
