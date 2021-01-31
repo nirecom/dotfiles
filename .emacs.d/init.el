3b9a0c8a4b40c00ccf9623c30712e216b3b5e235
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2021-01-31 12:32:19 +0900

    get .config directory shareable with other tools

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -161,8 +161,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (php-mode dockerfile-mode markdown-mode uuidgen solarized-theme markdown-preview-mode))))
+   '(php-mode dockerfile-mode markdown-mode uuidgen solarized-theme markdown-preview-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
