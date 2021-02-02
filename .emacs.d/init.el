18a43b268930ad677cf91647907c5d3cdf6e3406
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-02-03 06:31:24 +0900

    assigned backslash key

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,7 @@
+;; Define Keys
+;; it does not work so far...?
+(define-key global-map [?¥] [?\\]) ;; type backslash instead of yen sign
+
 ;; Initialize package
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
@@ -197,8 +201,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (php-mode dockerfile-mode markdown-mode uuidgen solarized-theme markdown-preview-mode))))
+   '(php-mode dockerfile-mode markdown-mode uuidgen solarized-theme markdown-preview-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
