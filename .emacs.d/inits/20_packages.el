a9a38c39074ff993c7ab386071b352721eedf429
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2021-02-06 12:23:34 +0900

    move auto package installation from package-install.el to 20_packages.el

--- .emacs.d/inits/20_packages.el
+++ .emacs.d/inits/20_packages.el
@@ -1,4 +1,10 @@
+;;; 20_package.el --- Initialize packages / Install packages if not exist
+;;; Commentary:
+;;; Code:
+
+;;
 ;; Initialize packages
+;;
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
@@ -9,3 +15,48 @@
 
 ;; configure load path (other than .emacs.d)
 ;(add-to-list 'load-path "~/.emacs.d/packages")
+
+;;
+;; Install packages if not exist
+;;
+(require 'cl)
+
+(defvar installing-package-list
+    '(
+    package-utils ; upgrade packages
+    init-loader ; load separated init.el files
+    auto-async-byte-compile ; automatically compiles el
+    git-gutter+
+    ;; completion
+    company ; company mode
+    ivy counsel swiper
+    ;; flycheck and related package
+    flycheck add-node-modules-path
+    ;; editorconfig
+    editorconfig
+    ;; several modes
+    dockerfile-mode
+    php-mode
+    smart-mode-line
+    terraform-mode
+    ;; markdown-preview-mode
+    markdown-mode
+    markdown-preview-mode websocket web-server uuidgen
+    ;; kotlin-mode
+    kotlin-mode flycheck-kotlin
+    ;; Web / JSX
+    web-mode rjsx-mode
+    ;; yaml
+    yaml-mode flymake-easy flymake-yaml
+))
+
+(let ((not-installed (loop for x in installing-package-list
+                         when (not (package-installed-p x))
+                         collect x)))
+    (when not-installed
+        (package-refresh-contents)
+        (dolist (pkg not-installed)
+            (package-install pkg))))
+
+(provide '20_packages)
+;;; 20_packages.el ends here
