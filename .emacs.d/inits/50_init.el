dd6ca274b28483ce17de06b6cce811c55382977a
Hideaki Nire (nirecom) <12681101+nirecom@users.noreply.github.com>
2021-02-07 22:51:19 +0900

    tag auto closing

--- .emacs.d/inits/50_init.el
+++ .emacs.d/inits/50_init.el
@@ -72,6 +72,12 @@
   '(lambda ()
     (add-to-list 'web-mode-comment-formats '("jsx" . "//" ))))
 
+; auto close tags with > and </
+(setq web-mode-enable-auto-closing t)
+(setq web-mode-enable-auto-pairing t)
+(setq web-mode-auto-close-style 2)
+(setq web-mode-tag-auto-close-style 2)
+
 ;;
 ;; Markdown
 ;;
