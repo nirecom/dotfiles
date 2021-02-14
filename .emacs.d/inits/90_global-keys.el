97aa18e9a344bb5166d6a47532a8d95627bff722
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-02-14 20:56:51 +0900

    just beautifying

--- .emacs.d/inits/90_global-keys.el
+++ .emacs.d/inits/90_global-keys.el
@@ -1,7 +1,9 @@
 ;;; 90_global-keys.el --- Keyboard Definitions
 ;;; Commentary:
 ;;; Code:
+
 ;; Define Keys
 (define-key global-map [?¥] [?\\]) ;; (macOS) type backslash instead of yen sign. may not work...?
 (global-set-key (kbd "<f12>") 'indent-region)
+
 ;;; 90_global-keys ends here
